ea5fd68e444ed022a366a7ed1eb21aadb1dfbb9d
Manish Devrani <79859472+manish591@users.noreply.github.com>
2021-07-11 17:39:30 +0530

    style

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import "./styles.css";
 export default function App() {
   const [message, setMessage] = useState("");
   let sum = 0;
+  let luckyNumber;
 
   let inputValue;
+
+  function setLucky(e) {
+    luckyNumber = e.target.value;
+  }
+
   function changeInput(e) {
     inputValue = e.target.value;
   }
@@ -21,7 +27,7 @@ export default function App() {
       sum += Number(date);
     });
 
-    if (sum % 7 === 0) {
+    if (sum % Number(luckyNumber) === 0) {
       setMessage("This is your lucky number!");
     } else {
       setMessage("This is not your lucky number!");
@@ -31,12 +37,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>Is Your Birthday Lucky?</h1>
-      <h2>
+      <p>
         This is a react js application in which it tests whether your birthday
-        is lucky or not based on certain calulation?
-      </h2>
+        is lucky or not based on certain calulation.
+      </p>
       <small>enter your date of birth in ddmmyyyy format</small>
       <input onChange={changeInput} placeholder="ddmmyyyy" />
+      <small>enter your lucky number here</small>
+      <input onChange={setLucky} placeholder="number between 0 and 9" />
       <button onClick={showLeap}>Calculate</button>
       <h2>{message}</h2>
     </div>
